a1ff3e95dc92133c536d7e67229bddd888ab54c1
Mvc2004 <merly.velasquez@correounivalle.edu.co>
2023-10-03 20:29:02 -0500

    aguegue que preguntara el id

--- proyectM/src/ListicaC.java
+++ proyectM/src/ListicaC.java
@@ -1,34 +1,58 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 public class ListicaC {
 
-    private List<Ciudadano> listaCiudadanos;
+    private List<Candidato> listaCandidatos;
 
     public ListicaC() {
-        listaCiudadanos = new ArrayList<>();
+        listaCandidatos = new ArrayList<>();
     }
 
-    public void agregarCiudadano(Ciudadano persona) {
-        listaCiudadanos.add(persona);
+    public void agregarCandidato(Candidato candidato) {
+        listaCandidatos.add(candidato);
     }
 
-    public void eliminarPersonaPorId(int id) {
-        Iterator<Ciudadano> iterator = listaCiudadanos.iterator();
+    public void eliminarCandidato(int id) {
+        Iterator<Candidato> iterator = listaCandidatos.iterator();
         while (iterator.hasNext()) {
-            Ciudadano persona = iterator.next();
-            if (persona.getId() == id) {
-                iterator.remove(); // Elimina la persona
-                System.out.println("Persona con ID " + id + " eliminada.");
-                return; 
+            Candidato candidato = iterator.next();
+            if (candidato.getId() == id) {
+                iterator.remove();
+                System.out.println("Candidato con ID " + id + " eliminado.");
+                return;
             }
         }
-        System.out.println("No se encontró ninguna persona con ID " + id + ".");
+        System.out.println("No se encontró ningún candidato con el ID " + id + ".");
     }
 
-    public void agregarPersona(Ciudadano ciudadano) {
+    public void mostrarCandidatos() {
+        System.out.println("Lista de candidatos:");
+        for (Candidato candidato : listaCandidatos) {
+            System.out.println(candidato);
+        }
     }
 
+    public static void main(String[] args) {
+        ListicaC lista = new ListicaC();
+        lista.agregarCandidato(new Candidato(1, "John"));
+        lista.agregarCandidato(new Candidato(2, "Doe"));
+        lista.agregarCandidato(new Candidato(3, "Alice"));
+        lista.mostrarCandidatos();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa el ID del candidato que deseas eliminar: ");
+        int idEliminar = scanner.nextInt();
+        scanner.nextLine(); // Limpiar el tin
+
+        lista.eliminarCandidato(idEliminar);
+        lista.mostrarCandidatos();
+
+        scanner.close(); // se cierra el scanner pa que funcione bien porque hay varios scanner
+    }
 }
+
+
 
