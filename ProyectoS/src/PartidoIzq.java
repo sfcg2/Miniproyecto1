038c14c94d88fe33587424bc2f58ca11224a57ea
Alejandra Giraldo <117177584+alosgi@users.noreply.github.com>
2023-10-05 09:54:29 -0500

    Revert "Sofia"

--- ProyectoS/src/PartidoIzq.java
+++
@@ -1,4 +0,0 @@
-public enum PartidoIzq {
-    Mais, Aico, ColombiaHumana, EsperanzaDemocratica, PoloDemocratico, PartidoComunes, 
-    PartidoComunista, PartidodelTrabajo, DignidadyCompromiso, UP, TodosSomosColombia,   
-}
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-
 # Miniproyecto1
 Realización de un CRUD
 # Sofia_Castillo_Giraldo
 # Alejandra_Osorio_Giraldo
 # Merly_Velasquez_Cortez 
-
